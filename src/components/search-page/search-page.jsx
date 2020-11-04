3979b898521a111e93c3dfb502610a900284f6a5
gheezl <gheezl25@gmail.com>
2020-11-04 13:04:04 -0500

    Adding more to the search page

--- src/components/search-page/search-page.jsx
+++ src/components/search-page/search-page.jsx
@@ -6,12 +6,15 @@ import "./search-page.css"
 const SearchPage = () => {
     return (
         <Fragment>
-            <div className="input-border" >
-                <input placeholder="Search Star Wars Information" className="input-form" />
-                <button className="search-button" >Search</button>
-            </div>
-            <div className="question-mark-border">
-                <h2>❓</h2>
+            <div className="input-border">
+                <div className="question-mark-border">
+                    <h2>❓</h2>
+                </div>
+                <div className="search-border">
+
+                    <input placeholder="Search Star Wars Information" className="input-form" />
+                    <button className="search-button" >Search</button>
+                </div>
             </div>
         </Fragment>
     )
