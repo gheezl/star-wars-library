903e79fdbd64de02cab4926d58f04809b0ea19ac
gheezl <gheezl25@gmail.com>
2020-11-05 17:36:47 -0500

    Solved the key error

--- src/components/card/card.jsx
+++ src/components/card/card.jsx
@@ -4,12 +4,13 @@ import "./card.css"
 
 
 const Card = ({ individualData }) => {
+
     return (
         <Fragment>
             {
                 individualData.map(data => {
                     return (
-                        <h2>{data.name}</h2>
+                        <h2 key={Math.floor(Math.random())} >{data.name}</h2>
                     )
                 })}
         </Fragment>
